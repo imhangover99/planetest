5e20416730760b3caa5aac03ccbfc829c31ae824
imhangover99 <imhangover@protonmail.com>
2020-03-29 19:19:56 +0000

    netlify

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-
+a=["qweqweqweqweqwe", "12654987"]
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', a=a)
+
 
 
 
