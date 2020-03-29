914884231c9f4436ab469b10aa365e258cb9278b
imhangover99 <imhangover@protonmail.com>
2020-03-29 19:52:56 +0000

    .

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-a=["qweqweqweqweqwe", "12654987"]
+
 @app.route('/')
 def index():
-	return render_template('index.html', a=a)
+	return render_template('index.html')
 
 
 
